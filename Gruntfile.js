6f402c6a59d26b3348b137fa23fac629eb8d68bd
yananlz <1540356239@qq.com>
2021-04-03 16:57:44 +0800

    css

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports=function(grunt){
+  grunt.initConfig({
+      csslint:{
+      options:{
+        csslintrc:"./.csslintrc"
+      },
+      src:["*.css"]
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-csslint");
+  grunt.registerTask("default",["csslint"]);
+}
